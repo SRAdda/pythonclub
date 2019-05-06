a76c17b4601a85018397aa0923d9801ef563a983
Sally <46608728+SRAdda@users.noreply.github.com>
2019-05-06 14:08:30 -0700

    Adds Resource View

--- pythonclub/clubapp/urls.py
+++ pythonclub/clubapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns=[
     path('', views.index,name='index'),
     path('gettitles/', views.gettitles, name='titles'),
+    path('getevents/', views.getevents, name='events'),
+    path('details/<int:id>', views.eventdetail, name='eventdetail'),
+    path('getresources/', views.getresources, name='resources'),
 
 ]
--- pythonclub/clubapp/views.py
+++ pythonclub/clubapp/views.py
@@ -6,7 +6,18 @@ def index(request):
     return render(request, 'clubapp/index.html')
 
 def gettitles(request):
-    title_list=MeetingTitle.objects.all()
-    return render(request, 'clubapp/titles.html' ,{'title_list' : title_list})
+    titles_list=Meeting.objects.all()
+    return render(request, 'clubapp/titles.html' ,{'titles_list' : titles_list})
 
-    
+def getevents(request):
+    events_list=Event.objects.all()
+    return render(request, 'clubapp/events.html', {'events_list': events_list})    
+
+def eventdetail(request, id):
+    detail=get_object_or_404(Event, pk=id)
+    context={ 'details' : details, }
+    return render(request, 'clubapp/eventdetail.html', context=context)
+
+def getresources(request, id):
+    resources_list=Resources.objects.all()
+    return render(request, 'clubapp/resources.html', {'resources_list': resources_list})
