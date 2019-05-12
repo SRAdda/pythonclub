9b767c4e6ff3a63f5ec68ed42447dcb61e026fed
Sally <46608728+SRAdda@users.noreply.github.com>
2019-05-12 10:00:38 -0700

    Adds tests to complete Assignment 7

--- pythonclub/clubapp/tests.py
+++ pythonclub/clubapp/tests.py
@@ -11,17 +11,29 @@ class MeetingTest(TestCase):
         meeting=Meeting(meetingtitle='monthly')
         self.assertEqual(str(meeting), meeting.meetingtitle)
 
+    def test_tablename(self):
+        self.assertEqual(str(Meeting._meta.db_table), 'clubapp_meeting')
+
 class ResourceTest(TestCase):
     def test_stringOutput(self):
         resource=Resource(resourcename='')
         self.assertEqual(str(resource), resource.resourcename)
+
+    def test_tablename(self):
+        self.assertEqual(str(Resource._meta.db_table), 'resource')
 
 class EventTest(TestCase):
     def test_stringOutput(self):
         event=Event(eventtitle='gala')
         self.assertEqual(str(event), event.eventtitle)
 
+    def test_tablename(self):
+        self.assertEqual(str(Event._meta.db_table), 'clubapp_event')
+
 class TestMeetingMinutes(TestCase):
     def test_view_url_accessible_by_name(self):
         response = self.client.get(reverse('meetingminutes'))
         self.assertEqual(response.status_code, 200)
+
+    def test_tablename(self):
+        self.assertEqual(str(MeetingMinutes._meta.db_table), 'attendance')
